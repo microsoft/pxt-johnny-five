ed9793da72ab680dcf616414658153b4ada74815
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-04-19 08:34:00 -0700

    removing deploy

--- editor/extension.ts
+++ editor/extension.ts
@@ -4,10 +4,6 @@ namespace pxt.editor {
     initExtensionsAsync = function (opts: pxt.editor.ExtensionOptions): Promise<pxt.editor.ExtensionResult> {
         pxt.debug('loading pxt-j5 target extensions...')
         const res: pxt.editor.ExtensionResult = {
-            deployCoreAsync: (resp: pxtc.CompileResult) => {
-                // send output files to node.js server to save locally
-                return Promise.resolve();
-            }
         };
         return Promise.resolve<pxt.editor.ExtensionResult>(res);
     }
